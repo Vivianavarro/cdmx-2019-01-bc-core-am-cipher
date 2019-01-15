3da9e1085b08aad9e089525feb8eb0d9a65f63f3
Viviana Navarro <nitoname@gmail.com>
2019-01-15 16:31:21 -0600

    agregué función de datos personales

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Eventos //
+
+const desplazamiento= document.getElementById("offsetsaltos");
+const mensajeUsuario=    document.getElementById("datosPersonales");
+
+document.getElementById("cifrar").addEventListener("click",()=>{
+
+mensajeUsuario.value
+
+desplazamiento.value
+
+window.cipher.enconde(mensajeUsuario,desplazamiento)
+
+
+})
+
+
